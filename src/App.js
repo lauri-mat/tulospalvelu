aa54a8e4b70450d71d5894fce11141ec796a33d9
Lauri Matikainen <lauri_matikainen@hotmail.com>
2022-02-28 11:37:16 +0200

    tulospalvelu päivitettynä

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+// import gun from './gun.gif'
+// import putler from './putler.gif'
+// import gun2 from './gun2.gif'
 
-function App() {
+const App = () => {
+
+  const[newHomeTeam, setNewHomeTeam] = useState('')
+  const[newVisitorTeam, setNewVisitorTeam] = useState('')
+  const[newResult, setNewResult] = useState('')
+  const[results, setResults] = useState([]) 
+
+  useEffect(() => { 
+    console.log('effect') 
+    axios 
+    .get('/api/results') 
+    .then(response => { 
+    console.log('promise fulfilled') 
+    setResults(response.data) 
+    }) 
+    }, []) 
+    console.log('render', results.length, 'results')
+  
+  const addResult = (event) => {
+    event.preventDefault()
+    const resultObject = {
+      hometeam: newHomeTeam,
+      visitorteam: newVisitorTeam,
+      result: newResult,
+      id: results.length + 1,
+    }
+    axios
+    .post('/api/results', resultObject)
+    .then(response => {
+    console.log('palvelimen vastaus: ', response)
+    console.log("results-taulukko: ", results)
+    setResults(results.concat(resultObject))
+    setNewResult('')
+    setNewHomeTeam('')
+    setNewVisitorTeam('')
+    })
+  }
+
+  const handleHomeTeamChange = (event) => {
+    setNewHomeTeam(event.target.value)
+  }
+
+  const handleVisitorTeamChange = (event) => {
+    setNewVisitorTeam(event.target.value)
+  }
+
+  const handleResultChange = (event) => {
+    setNewResult(event.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {console.log("tulokset: ", results)}
+      {/* {results.map(r => <li key={r.id}>{r.hometeam} vs. {r.visitorteam}: {r.result}</li>)} */}
+           
+      <div id="form">
+      {/* <img width="200px" src={gun2} alt="lataan..." />
+      <img src={putler} alt="lataan..." />
+      <img width="200px" src={gun} alt="lataan..." /> */}
+      <h2>Tulospalvelu</h2>
+      <form onSubmit={addResult}>
+        <table>
+          <tbody>
+          <tr>
+          <td>Nimi: </td><td><input value={newHomeTeam} onChange={handleHomeTeamChange} /></td>
+          </tr>
+          <tr>
+            <td>Tulos, kierros 1: </td><td><input value={newVisitorTeam} onChange={handleVisitorTeamChange} /></td>
+          </tr>
+          <tr>
+            <td>Tulos, kierros 2: </td><td className='result'><input value={newResult} onChange={handleResultChange} /></td>
+          </tr>
+          </tbody>   
+          </table>          
+        <button type="submit">Lähetä</button>
+        </form>
+        </div>
+        
+          <table>
+            <tbody>
+            <tr>
+          <th width="250px">Nimi</th><th width="250px">Tulos, kierros 1</th><th>Tulos, kierros 2</th>
+          </tr><tr>
+          <td>{results.map(r => <p key={r.id}>{r.hometeam}</p>)}{newHomeTeam}</td>
+          <td>{results.map(r => <p key={r.id}>{r.visitorteam}</p>)}{newVisitorTeam}</td>
+          <td>{results.map(r => <p key={r.id}>{r.result}</p>)}{newResult}</td>
+        </tr>
+        </tbody>
+        </table>
+        
+        
     </div>
   );
 }
---
+++ src/components/results.js
@@ -0,0 +1,22 @@
+const results = [
+    {
+        hometeam: "Yltäyliskyllästetyt",
+        visitorteam: "Vähemmistön enemmistö",
+        result: "1-0",
+        id: 0
+    },
+    {
+        hometeam: "Suuret Johtajat",
+        visitorteam: "Taiteelliset harhaanjohtajat",
+        result: "0-2",
+        id: 1
+    },
+    {
+        hometeam: "Valon Vartijat",
+        visitorteam: "Synkät valtiaat",
+        result: "1-1",
+        id: 2
+    }
+  ]
+
+export default results;
