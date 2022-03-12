0e865395956a11cad61b2a3adb7a369eb435f28f
Lauri Matikainen <lauri_matikainen@hotmail.com>
2022-03-12 15:01:31 +0200

    muokattu jutut pois

---
+++ build/precache-manifest.ed81943c2726370651d86001171006a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce6c49b966caf9bf138b",
+    "url": "/static/css/main.a4bacfa0.chunk.css"
+  },
+  {
+    "revision": "ce6c49b966caf9bf138b",
+    "url": "/static/js/main.ce6c49b9.chunk.js"
+  },
+  {
+    "revision": "3d6fb49f4be1340aaf12",
+    "url": "/static/js/1.3d6fb49f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4e72aa5f185b4238dd5444866b575b96",
+    "url": "/index.html"
+  }
+];
